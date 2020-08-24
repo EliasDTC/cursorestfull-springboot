534df57773483cc5265992524d44fbf35170e483
Elias <elias.della@hotmail.com>
2020-08-24 20:17:26 -0300

    meu primeiro commit

---
+++ src/main/java/com/example/cursorestfullspringboot/CursorestfullSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.cursorestfullspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursorestfullSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursorestfullSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cursorestfullspringboot/CursorestfullSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cursorestfullspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursorestfullSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
